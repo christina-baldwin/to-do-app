acbcbcbed57eb235fdd8983deae5055478fa4846
christina-baldwin <christina.baldwin13@yahoo.com>
2025-05-23 13:04:48 +0200

    removing repeated code

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -12,43 +12,6 @@ const TodoList = () => {
     return true;
   });
 
-  if (todos.length === 0)
-    return (
-      <div className="flex items-center flex-col gap-10 min-w-sm">
-        <div className="flex flex-row gap-8">
-          <p>All tasks: {todos.length}</p>
-          <p>
-            Incomplete tasks:{" "}
-            {todos.filter((todo) => todo.complete === false).length}
-          </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p>Filter tasks by:</p>
-          <div className="flex gap-2">
-            <button
-              className="border border-solid border-blue-200  rounded-md cursor-pointer p-2  hover:bg-blue-200 hover:text-gray-800 "
-              onClick={() => setFilter("all")}
-            >
-              All
-            </button>
-            <button
-              className="border border-solid border-blue-200  rounded-md cursor-pointer p-2  hover:bg-blue-200 hover:text-gray-800 "
-              onClick={() => setFilter("completed")}
-            >
-              Complete
-            </button>
-            <button
-              className="border border-solid border-blue-200 rounded-md cursor-pointer p-2  hover:bg-blue-200 hover:text-gray-800 "
-              onClick={() => setFilter("uncompleted")}
-            >
-              Incomplete
-            </button>
-          </div>
-        </div>
-        <p>You have no tasks yet. Add a to-do to get started!</p>
-      </div>
-    );
-
   return (
     <div className="flex items-center flex-col gap-10 min-w-sm">
       <div className="flex flex-row gap-8">
@@ -62,13 +25,13 @@ const TodoList = () => {
         <p>Filter tasks by:</p>
         <div className="flex gap-2">
           <button
-            className="border border-solid border-blue-200 rounded-md cursor-pointer p-2  hover:bg-blue-200 hover:text-gray-800 "
+            className="border border-solid border-blue-200  rounded-md cursor-pointer p-2  hover:bg-blue-200 hover:text-gray-800 "
             onClick={() => setFilter("all")}
           >
             All
           </button>
           <button
-            className="border border-solid border-blue-200 rounded-md cursor-pointer p-2  hover:bg-blue-200 hover:text-gray-800 "
+            className="border border-solid border-blue-200  rounded-md cursor-pointer p-2  hover:bg-blue-200 hover:text-gray-800 "
             onClick={() => setFilter("completed")}
           >
             Complete
@@ -81,9 +44,11 @@ const TodoList = () => {
           </button>
         </div>
       </div>
-      {filteredTodos.map((todo) => (
-        <ToDoMessage key={todo.id} {...todo} />
-      ))}
+      {todos.length === 0 ? (
+        <p>You have no tasks yet. Add a to-do to get started!</p>
+      ) : (
+        filteredTodos.map((todo) => <ToDoMessage key={todo.id} {...todo} />)
+      )}
     </div>
   );
 };
