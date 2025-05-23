2b931c29db3c522b395a0e693f3064e61acb348e
christina-baldwin <christina.baldwin13@yahoo.com>
2025-05-23 13:11:45 +0200

    heading size change

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import TodoList from "./components/TodoList";
 
 export const App = () => {
   return (
-    <div className="flex items-center min-h-screen flex-col gap-8 p-8 text-blue-200 bg-gray-800">
-      <h1 className="text-4xl">My To-Do List</h1>
+    <div className="flex items-center min-h-screen flex-col gap-10 p-8 text-blue-200 bg-gray-800">
+      <h1 className="text-5xl font-bold">My To-Do List</h1>
       <ToDoForm />
       <TodoList />
     </div>
